304df63b0141b77ecb56e7a826af2ad839ec7cc3
明磊 <1243055406@qq.com>
2017-08-15 22:16:39 +0800

    http error

--- views.py
+++ views.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'MingLei Ji'
-from webapp import Route
+from webapp import Route, ctx
 
 index = Route()
 
 
 @index.get('/')
 def home():
-    return '<h1>hello world</h1>'
+    request = ctx.request
+    addr = request['REMOTE_ADDR']
+    return '<h1>hello world</h1>' + '\n the ip you from ' + addr
 
 
 @index.get('/:name/')
--- webapp.py
+++ webapp.py
@@ -1,8 +1,192 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'MingLei Ji'
 import re
+import sys
+import threading
+
+if sys.version > '3':
+    unicode = str
+
+ctx = threading.local()
 
 _re_route = re.compile(r'(\:[a-zA-Z_]\w*)')
+
+_RESPONSE_STATUSES = {
+    # Informational
+    100: 'Continue',
+    101: 'Switching Protocols',
+    102: 'Processing',
+
+    # Successful
+    200: 'OK',
+    201: 'Created',
+    202: 'Accepted',
+    203: 'Non-Authoritative Information',
+    204: 'No Content',
+    205: 'Reset Content',
+    206: 'Partial Content',
+    207: 'Multi Status',
+    226: 'IM Used',
+
+    # Redirection
+    300: 'Multiple Choices',
+    301: 'Moved Permanently',
+    302: 'Found',
+    303: 'See Other',
+    304: 'Not Modified',
+    305: 'Use Proxy',
+    307: 'Temporary Redirect',
+
+    # Client Error
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    402: 'Payment Required',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Not Allowed',
+    406: 'Not Acceptable',
+    407: 'Proxy Authentication Required',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    412: 'Precondition Failed',
+    413: 'Request Entity Too Large',
+    414: 'Request URI Too Long',
+    415: 'Unsupported Media Type',
+    416: 'Requested Range Not Satisfiable',
+    417: 'Expectation Failed',
+    418: "I'm a teapot",
+    422: 'Unprocessable Entity',
+    423: 'Locked',
+    424: 'Failed Dependency',
+    426: 'Upgrade Required',
+
+    # Server Error
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
+    505: 'HTTP Version Not Supported',
+    507: 'Insufficient Storage',
+    510: 'Not Extended',
+}
+
+_RE_RESPONSE_STATUS = re.compile(r'^\d\d\d(\ [\w\ ]+)?$')
+
+_RESPONSE_HEADERS = (
+    'Accept-Ranges',
+    'Age',
+    'Allow',
+    'Cache-Control',
+    'Connection',
+    'Content-Encoding',
+    'Content-Language',
+    'Content-Length',
+    'Content-Location',
+    'Content-MD5',
+    'Content-Disposition',
+    'Content-Range',
+    'Content-Type',
+    'Date',
+    'ETag',
+    'Expires',
+    'Last-Modified',
+    'Link',
+    'Location',
+    'P3P',
+    'Pragma',
+    'Proxy-Authenticate',
+    'Refresh',
+    'Retry-After',
+    'Server',
+    'Set-Cookie',
+    'Strict-Transport-Security',
+    'Trailer',
+    'Transfer-Encoding',
+    'Vary',
+    'Via',
+    'Warning',
+    'WWW-Authenticate',
+    'X-Frame-Options',
+    'X-XSS-Protection',
+    'X-Content-Type-Options',
+    'X-Forwarded-Proto',
+    'X-Powered-By',
+    'X-UA-Compatible',
+)
+
+_HEADER_X_POWERED_BY = ('X-Powered-By', 'python/webapp')
+
+
+class HttpError(Exception):
+    def __init__(self, code):
+        super(HttpError, self).__init__()
+        self.status = '%d %s' % (code, _RESPONSE_STATUSES[code])
+
+    def header(self, name, value):
+        if not hasattr(self, '_headers'):
+            self._headers = [_HEADER_X_POWERED_BY]
+        self._headers.append((name, value))
+
+    @property
+    def headers(self):
+        if hasattr(self, '_headers'):
+            return self._headers
+        return []
+
+    def __str__(self):
+        return self.status
+
+    __repr__ = __str__
+
+
+class RedirectError(HttpError):
+    def __init__(self, code, location):
+        super(RedirectError, self).__init__(code)
+        self.location = location
+
+    def __str__(self):
+        return '%s, %s' % (self.status, self.location)
+
+    __repr__ = __str__
+
+
+def badrequest():
+    return HttpError(400)
+
+
+def unauthorized():
+    return HttpError(401)
+
+
+def forbidden():
+    return HttpError(403)
+
+
+def notfound():
+    return HttpError(404)
+
+
+def conflict():
+    return HttpError(409)
+
+
+def internalerror():
+    return HttpError(500)
+
+
+def redirect(location):
+    return RedirectError(301, location)
+
+
+def found(location):
+    return RedirectError(302, location)
+
+
+def seeother(location):
+    return RedirectError(303, location)
 
 
 def _build_regex(path):
@@ -26,6 +210,18 @@ def _build_regex(path):
         is_var = not is_var
     re_list.append('$')
     return ''.join(re_list)
+
+
+def _to_str(s):
+    if isinstance(s, unicode):
+        return s
+    return s.decode('utf-8')
+
+
+def _to_byte(s):
+    if isinstance(s, bytes):
+        return s
+    return s.encode('utf-8')
 
 
 class Route(object):
@@ -97,6 +293,7 @@ class WSGIApplication(object):
                 if args:
                     # 传参调用处理函数
                     return fn(*(args.groups()))
+            raise notfound()
         if methods == 'POST':
             fn = self._post_static.get(url, None)
             if fn:
@@ -105,7 +302,8 @@ class WSGIApplication(object):
                 args = re_path.match(url)
                 if args:
                     return fn(*(args.groups()))
-        return '404'
+            raise notfound()
+        raise badrequest()
 
     def run(self):
         # 使用python内置的wsgi服务
@@ -116,11 +314,33 @@ class WSGIApplication(object):
 
     def application(self, environ, start_response):
         # 请求处理
+        ctx.request = environ
         request_method = environ['REQUEST_METHOD']
         path_info = environ['PATH_INFO']
-        # 匹配处理程序
-        r = self.match(path_info, request_method)
-        status = '200 OK'
-        headers = [('Content-Type', 'text/html; charset=utf-8')]
-        start_response(status, headers)
-        return [r.encode('utf-8')]
+        # py3 需要转换编码
+        if sys.version > '3':
+            path_info = path_info.encode('iso-8859-1').decode('utf8')
+        try:
+            # 匹配处理程序
+            r = self.match(path_info, request_method)
+            r = _to_byte(r)
+            status = '200 OK'
+            headers = [('Content-Type', 'text/html; charset=utf-8')]
+            start_response(status, headers)
+            return [r]
+        except RedirectError as e:
+            # 重定向
+            start_response(e.status, [{('Location', e.location)}])
+            return []
+        except HttpError as e:
+            # http error
+            error = '<html><body><h1>' + e.status + '</h1></body></html>'
+            error = _to_byte(error)
+            start_response(e.status, e.headers)
+            return [error]
+        except Exception as e:
+            # 系统错误
+            error = '<html><body><h1>500 Internal Server Error</h1></body></html>'
+            error = _to_byte(error)
+            start_response('500 Internal Server Error', [])
+            return [error]
