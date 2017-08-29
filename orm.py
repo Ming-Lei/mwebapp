6d77535ffb80bb4921b0e0232ecbcdfdbc99696a
Ming-Lei <1243055406@qq.com>
2017-08-29 17:09:56 +0800

    orm

---
+++ orm.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'MingLei Ji'
+import time
+
+import db
+
+
+class Field(object):
+    # 基础字段类 定义字段的属性
+    _count = 0
+
+    def __init__(self, **kw):
+        self.name = kw.get('name', None)
+        self._default = kw.get('default', None)
+        self.primary_key = kw.get('primary_key', False)
+        self.nullable = kw.get('nullable', False)
+        self.updatable = kw.get('updatable', True)
+        self.insertable = kw.get('insertable', True)
+        self.ddl = kw.get('ddl', '')
+        self._order = Field._count
+        Field._count = Field._count + 1
+
+    @property
+    def default(self):
+        d = self._default
+        return d() if callable(d) else d
+
+    def __str__(self):
+        s = ['<%s:%s,%s,default(%s),' % (self.__class__.__name__, self.name, self.ddl, self._default)]
+        self.nullable and s.append('N')
+        self.updatable and s.append('U')
+        self.insertable and s.append('I')
+        s.append('>')
+        return ''.join(s)
+
+
+class StringField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'varchar(255)'
+        super(StringField, self).__init__(**kw)
+
+
+class IntegerField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = 0
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bigint'
+        super(IntegerField, self).__init__(**kw)
+
+
+class FloatField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = 0.0
+        if not 'ddl' in kw:
+            kw['ddl'] = 'real'
+        super(FloatField, self).__init__(**kw)
+
+
+class BooleanField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = False
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bool'
+        super(BooleanField, self).__init__(**kw)
+
+
+class TextField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'text'
+        super(TextField, self).__init__(**kw)
+
+
+class BlobField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'blob'
+        super(BlobField, self).__init__(**kw)
+
+
+class VersionField(Field):
+    def __init__(self, name=None):
+        super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
+
+
+_triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
+
+
+def _gen_sql(table_name, mappings):
+    # 生成表结构语句
+    pk = None
+    sql = ['-- generating SQL for %s:' % table_name, 'create table `%s` (' % table_name]
+    for f in sorted(mappings.values(), key=lambda x: x._order):
+        if not hasattr(f, 'ddl'):
+            raise Exception('no ddl in field "%s".' % f)
+        ddl = f.ddl
+        nullable = f.nullable
+        if f.primary_key:
+            pk = f.name
+        sql.append(nullable and '  `%s` %s,' % (f.name, ddl) or '  `%s` %s not null,' % (f.name, ddl))
+    sql.append('  primary key(`%s`)' % pk)
+    sql.append(');')
+    return '\n'.join(sql)
+
+
+class ModelMetaclass(type):
+    # 元类 控制model的生成
+    def __new__(cls, name, bases, attrs):
+        # skip base Model class:
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # store all subclasses info:
+        if not hasattr(cls, 'subclasses'):
+            cls.subclasses = {}
+        if not name in cls.subclasses:
+            cls.subclasses[name] = name
+
+        mappings = dict()
+        primary_key = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                if not v.name:
+                    v.name = k
+                # check duplicate primary key:
+                if v.primary_key:
+                    if primary_key:
+                        raise TypeError('Cannot define more than 1 primary key in class: %s' % name)
+                    if v.updatable:
+                        v.updatable = False
+                    if v.nullable:
+                        v.nullable = False
+                    primary_key = v
+                mappings[k] = v
+        # check exist of primary key:
+        if not primary_key:
+            raise TypeError('Primary key not defined in class: %s' % name)
+        for k in mappings.keys():
+            attrs.pop(k)
+        if not '__table__' in attrs:
+            attrs['__table__'] = name.lower()
+        attrs['__mappings__'] = mappings
+        attrs['__primary_key__'] = primary_key
+        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
+        for trigger in _triggers:
+            if not trigger in attrs:
+                attrs[trigger] = None
+        return type.__new__(cls, name, bases, attrs)
+
+
+def with_metaclass(meta, *bases):
+    # 兼容2和3的元类  见 py2 future.utils.with_metaclass
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d)
+
+    return metaclass('temporary_class', None, {})
+
+
+class Model(with_metaclass(ModelMetaclass, dict)):
+    # model 类 调用db模块 绑定sql方法
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    @classmethod
+    def get(cls, pk):
+        d = db.select_one('select * from %s where %s=?' % (cls.__table__, cls.__primary_key__.name), pk)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_first(cls, where, *args):
+        d = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_all(cls, *args):
+        L = db.select('select * from `%s`' % cls.__table__)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def find_by(cls, where, *args):
+        L = db.select('select * from `%s` %s' % (cls.__table__, where), *args)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def count_all(cls):
+        return db.select_int('select count(`%s`) from `%s`' % (cls.__primary_key__.name, cls.__table__))
+
+    @classmethod
+    def count_by(cls, where, *args):
+        return db.select_int('select count(`%s`) from `%s` %s' % (cls.__primary_key__.name, cls.__table__, where),
+                             *args)
+
+    def update(self):
+        self.pre_update and self.pre_update()
+        L = []
+        args = []
+        for k, v in self.__mappings__.iteritems():
+            if v.updatable:
+                if hasattr(self, k):
+                    arg = getattr(self, k)
+                else:
+                    arg = v.default
+                    setattr(self, k, arg)
+                L.append('`%s`=?' % k)
+                args.append(arg)
+        pk = self.__primary_key__.name
+        args.append(getattr(self, pk))
+        db.update('update `%s` set %s where %s=?' % (self.__table__, ','.join(L), pk), *args)
+        return self
+
+    def delete(self):
+        self.pre_delete and self.pre_delete()
+        pk = self.__primary_key__.name
+        args = (getattr(self, pk),)
+        db.update('delete from `%s` where `%s`=?' % (self.__table__, pk), *args)
+        return self
+
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params = {}
+        for k, v in self.__mappings__.iteritems():
+            if v.insertable:
+                if not hasattr(self, k):
+                    setattr(self, k, v.default)
+                params[v.name] = getattr(self, k)
+        db.insert('%s' % self.__table__, **params)
+        return self
+
+
+if __name__ == '__main__':
+    db.create_engine('root', 'password', 'test')
+
+
+    class User(Model):
+        id = IntegerField(primary_key=True, updatable=False)
+        name = StringField()
+        email = StringField(updatable=False)
+        passwd = StringField(default=lambda: '******')
+        last_modified = FloatField()
+
+        def pre_insert(self):
+            self.last_modified = time.time()
+
+
+    user_list = User.find_by('where id>?', 1000)
+    for user in user_list:
+        print(user)
